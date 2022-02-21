dfac51de0912d07e8a36c86350cab4b3a4a98a29
monsky31 <rajaikhsanhalomoan@gmail.com>
2022-02-21 13:18:20 +0700

    Menambahkan Database URL

--- config/config.js
+++ config/config.js
@@ -17,15 +17,11 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    use_environment: process.env.DB_URL,
     dialectOptions: {
-      // ssl: {
-      //   rejectUnauthorized: false,
-      // },
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 };
